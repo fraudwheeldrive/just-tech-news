c138556bce09305533e2f354c7d5ddd45a78a713
George Tsatalbasidis <adcore89@gmail.com>
2021-05-12 22:14:46 -0400

    post routes finished

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
 const User = require('./User');
 const Post = require('./post');
 
+// associations 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignkey: 'user_id',
+});
 module.exports = { User, Post };
